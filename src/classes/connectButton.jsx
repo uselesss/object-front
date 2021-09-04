9165d23315e0f9cd3d251253f9316ebba8151ccb
uselesss <dushebaa75@gmail.com>
2021-09-04 06:48:53 +0300

    Metamask transactions

--- src/classes/connectButton.jsx
+++ src/classes/connectButton.jsx
@@ -1,30 +1,85 @@
-import React from "react"; 
-import { useEtherBalance, useEthers, ContractCall, useContractCall} from '@usedapp/core'
-import PropTypes from 'prop-types'
-import {formatEther} from '@ethersproject/units'
-import { utils } from 'ethers'
-import { Contract } from '@ethersproject/contracts'
-import abi from '../abi/rentContract.json' 
+import { useStoreApi } from "./storeApi";
+import useWeb3 from "./useWeb3";
 
-export function ConnectWallet() {
-    const { activateBrowserWallet, account } = useEthers()
-    const etherBalance = useEtherBalance(account)
-  
-    const contractAbi = new utils.Interface(abi)
-    const contractAddress = '0xd2435293340E537BAcD2feD2f700B204C707518d'
-    const contract = new Contract(contractAddress, contractAbi)
+import { Button, TextField } from "@material-ui/core";
 
-    // let zxc = ContractCall(contractAbi, contractAddress, "landlord")
+function ConnectButton() {
+  const { balance, address, message, setAddress, setBalance } = useStoreApi();
+  const web3 = useWeb3();
 
-    return (
-    <div>
-        <div>
-        <button onClick={() => activateBrowserWallet()}>Connect</button>
-        </div>
-      {account && <p>Account: {account}</p>}
-      {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
+  // get user account on button click
+  const getUserAccount = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        web3.eth.getAccounts().then(accounts => {
+          setAddress(accounts[0]);
+          updateBalance(accounts[0]);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("Metamask extensions not detected!");
+    }
+  };
+
+  const updateBalance = async fromAddress => {
+    await web3.eth.getBalance(fromAddress).then(value => {
+      setBalance(web3.utils.fromWei(value, "ether"));
+    });
+  };
+
+  const sendTransaction = async e => {
+    e.preventDefault();
+    const amount = e.target[0].value;
+    const recipient = e.target[1].value;
+    await web3.eth.sendTransaction({
+      from: address,
+      to: recipient,
+      value: web3.utils.toWei(amount, "ether")
+    });
+    updateBalance(address);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+        <Button
+          onClick={() => getUserAccount()}
+          variant="outlined"
+          color="primary"
+        >
+          Connect your wallet
+        </Button>
+        {message ? (
+          <p>
+            <code>{message}</code>
+          </p>
+        ) : null}
+        <form onSubmit={e => sendTransaction(e)}>
+          <TextField
+            required
+            label="Amount"
+            inputProps={{ step: "any" }}
+            type="number"
+            variant="filled"
+          />
+          <TextField required label="Recipient Address" variant="filled" />
+          <Button
+            style={{ margin: "10px" }}
+            type="submit"
+            variant="outlined"
+            color="default"
+          >
+            Send Crypto
+          </Button>
+        </form>
+      </header>
     </div>
-  )
+  );
 }
 
-export default ConnectWallet;
+export default ConnectButton;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Request from './classes/request.jsx';
-import Map from './classes/map.jsx';
-import { ChainId, DAppProvider } from "@usedapp/core";
-import ConnectWallet from './classes/connectButton.jsx';
-
-const config = {
-  readOnlyChain: ChainId.Kovan,
-  readOnlyUrls: {
-    [ChainId.Kovan]: 'https://mainnet.infura.io/v3/912668c188114c6fb74af1746ada48c5',
-  },
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ConnectButton from "./classes/connectButton";
+import { StoreProvider } from "./classes/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <DAppProvider config={config}>
-      <ConnectWallet/>
-    </DAppProvider>
+    <StoreProvider>
+      <ConnectButton />
+    </StoreProvider>
   </React.StrictMode>,
-
-  document.getElementById('root')
-);  
+  document.getElementById("root")
+);
